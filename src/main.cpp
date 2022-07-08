3149259270c5a485542955710e6e2a1746b9a1d1
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2022-07-08 17:28:19 +0200

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+
+  digitalWrite(LED_BUILTIN, HIGH); // turn the led off
+  delay(1000);                     // wait for 1 sec
+  digitalWrite(LED_BUILTIN, LOW);  // turn the led on
+  delay(1000);                     // wait for 1 sec
 }
